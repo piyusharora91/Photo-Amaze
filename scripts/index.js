9e00101acbd20468308e97d628d555988194fb81
piyusharora91 <piyusharora596@gmail.com>
2023-01-26 15:32:51 +0530

    initial web files upload for the version 1.0

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+import records from './records.js';
+import createCard from './createcard.js';
+
+const rowContainer= document.querySelector('.row');
+const downloadBtnElements= document.querySelectorAll('.btn');
+
+const downloadImage= (e)=> {
+    const imgElement= e.target.parentElement.previousElementSibling;  //gets img element
+    const downloadBtn= e.target;
+    downloadBtn.download = `${imgElement.src}.png`;
+}
+
+document.addEventListener('DOMContentLoaded',() => {
+    Array.from(downloadBtnElements).forEach((btn)=> {
+        btn.addEventListener('click', (e)=> downloadImage(e));
+    });
+
+    Object.keys(records).forEach((record)=> {
+        const card= createCard(record, records[record]);
+        rowContainer.appendChild(card);
+    });
+});
