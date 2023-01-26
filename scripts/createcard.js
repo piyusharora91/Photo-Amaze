3c3262e1d85dd7457d8a30171e9f837b279f9836
piyusharora91 <piyusharora596@gmail.com>
2023-01-26 16:03:31 +0530

    card styling updates

--- scripts/createcard.js
+++ scripts/createcard.js
@@ -1,32 +1,33 @@
-const attributeSet= (target, attrType, value) => {
-    target.setAttribute(attrType, value);
-}
+const attributeSet = (target, attrType, value) => {
+  target.setAttribute(attrType, value);
+};
 
-const createCard= (name, obj)=> {
-    const card= document.createElement('div');
-    card.className = 'card col-xl-3 col-md-5 col-xs-12';
+const createCard = (name, obj) => {
+  const card = document.createElement("div");
+  card.className = "card col-xl-3 col-sm-5 col-xs-12";
 
-    const imgElement= document.createElement('img');
-    imgElement.classList.add('card-img-top');
-    attributeSet(imgElement, 'src', obj);
-    attributeSet(imgElement, 'alt', name);
-    attributeSet(imgElement, 'loading', 'lazy');
+  const imgElement = document.createElement("img");
+  imgElement.classList.add("card-img-top");
+  attributeSet(imgElement, "src", obj);
+  attributeSet(imgElement, "alt", name);
+  attributeSet(imgElement, "title", name);
+  attributeSet(imgElement, "loading", "lazy");
 
-    const cardBody= document.createElement('div');
-    cardBody.className = 'card-body';
-    const cardTitle= document.createElement('h5');
-    cardTitle.className = 'card-title';
-    cardTitle.textContent= name;
-    const downloadLink= document.createElement('a');
-    downloadLink.className= 'btn btn-primary';
-    downloadLink.textContent= 'Download Image';
-    attributeSet(downloadLink, 'href', obj);
-    attributeSet(downloadLink, 'download', obj.split('/')[2]);
+  const cardBody = document.createElement("div");
+  cardBody.className = "card-body";
+  const cardTitle = document.createElement("h5");
+  cardTitle.className = "card-title";
+  cardTitle.textContent = name;
+  const downloadLink = document.createElement("a");
+  downloadLink.className = "btn btn-primary";
+  downloadLink.textContent = "Download Image";
+  attributeSet(downloadLink, "href", obj);
+  attributeSet(downloadLink, "download", obj.split("/")[2]);
 
-    cardBody.append(cardTitle, downloadLink);
-    card.append(imgElement, cardBody);
+  cardBody.append(cardTitle, downloadLink);
+  card.append(imgElement, cardBody);
 
-    return card;
-}
+  return card;
+};
 
 export default createCard;
--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,29 @@
-import records from './records.js';
-import createCard from './createcard.js';
+import records from "./records.js";
+import createCard from "./createcard.js";
 
-const rowContainer= document.querySelector('.row');
-const downloadBtnElements= document.querySelectorAll('.btn');
+const rowContainer = document.querySelector(".row");
+const downloadBtnElements = document.querySelectorAll(".btn");
 
-const downloadImage= (e)=> {
-    const imgElement= e.target.parentElement.previousElementSibling;  //gets img element
-    const downloadBtn= e.target;
-    downloadBtn.download = `${imgElement.src}.png`;
-}
+const downloadImage = (e) => {
+  const imgElement = e.target.parentElement.previousElementSibling; //gets img element
+  const downloadBtn = e.target;
+  downloadBtn.download = `${imgElement.src}.png`;
+};
 
-document.addEventListener('DOMContentLoaded',() => {
-    Array.from(downloadBtnElements).forEach((btn)=> {
-        btn.addEventListener('click', (e)=> downloadImage(e));
+document.addEventListener("DOMContentLoaded", () => {
+  Array.from(downloadBtnElements).forEach((btn) => {
+    btn.addEventListener("click", (e) => downloadImage(e));
+  });
+
+  Object.keys(records).forEach((record) => {
+    const card = createCard(record, records[record]);
+    rowContainer.appendChild(card);
+  });
+
+  const imgElements = document.querySelectorAll(".card-img-top");
+  imgElements.forEach((imgElement) => {
+    imgElement.addEventListener("keydown", (e) => {
+      console.log(e);
     });
-
-    Object.keys(records).forEach((record)=> {
-        const card= createCard(record, records[record]);
-        rowContainer.appendChild(card);
-    });
+  });
 });
